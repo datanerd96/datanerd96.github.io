3e1e9ade2a1c6e827bbed5cef5f67a6dbe8475ec
Jedidja Kakuva <jedidja96@outlook.com>
2025-03-31 00:52:56 +0200

    Added new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,2 @@
-// Example: Smooth scroll for links
-// This will smoothly scroll to the target section when clicking a link
-
-document.querySelectorAll('a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault(); // Prevent default link behavior
-
-        // Smooth scroll to the element with the href target
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth' // Smooth scrolling effect
-        });
-    });
-});
-
+function toggleTheme() {
+    document.body.classList.toggle('dark');
